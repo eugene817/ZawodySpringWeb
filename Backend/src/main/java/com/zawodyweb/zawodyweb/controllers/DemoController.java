8d83fea3dc977d01b6004abd2db1beb49f76838a
Yauheni <luferchikz@gmail.com>
2025-11-29 23:31:39 +0100

    Controller

--- Backend/src/main/java/com/zawodyweb/zawodyweb/controllers/DemoController.java
+++ Backend/src/main/java/com/zawodyweb/zawodyweb/controllers/DemoController.java
@@ -1,17 +1,17 @@
-package com.zawodyweb.zawodyweb.controllers;
-
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/demo")
-public class DemoController {
-
-  @GetMapping("/demo")
-  @SecurityRequirement(name = "bearerAuth")
-  public String demo() {
-    return "Hello!";
-  }
-}
+//package com.zawodyweb.zawodyweb.controllers;
+//
+//import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/demo")
+//public class DemoController {
+//
+//  @GetMapping("/demo")
+//  @SecurityRequirement(name = "bearerAuth")
+//  public String demo() {
+//    return "Hello!";
+//  }
+//}
